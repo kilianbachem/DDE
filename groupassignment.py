d4a12a6134f23d5ebc3e4f4aaee0f2df1a8c22a2
kilianbachem <36304730+kilianbachem@users.noreply.github.com>
2024-04-19 17:03:48 +0200

    Tried to implement 'valid API test' feature, however failed therefore added as todo

--- groupassignment.py
+++ groupassignment.py
@@ -1,3 +1,5 @@
+
+# Todo - LangChainDeprecationWarning: Importing LLMs from langchain is deprecated. Importing from langchain will no longer be supported as of langchain==0.2.0. Please import from langchain-community instead: `from langchain_community.llms import OpenAI` To install langchain-community run `pip install -U langchain-community`.
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
@@ -15,6 +17,26 @@ st.caption("Powered by OpenAI's GPT-3")
 st.divider()
 st.markdown("See the generated elevator pitch below:")
 
+# Function that proofs if the entered key is a valid OpenAI API key
+# Todo - check if it is better to have the "official" error message from OpenAI
+# Todo - fix bug that the API key is displayed as invaild eventhough it is valid
+# def test_api_key(api_key):
+#     """ Function to test if the provided API key is valid by making a simple API call. """
+#     try:
+#         chat_api = ChatOpenAI(api_key=api_key)
+#         response = chat_api.invoke(["Test"])  # Simple test message
+#         # Check if the response is successful
+#         if response.status_code == 200:
+#             st.success("API Key is valid. Generating elevator pitch...")
+#             return True
+#         else:
+#             st.warning("Invalid API Key. Please check your API Key and try again.")
+#             return False
+#     except Exception as e:
+#         # This block catches any kind of exception and logs a generic warning
+#         st.warning("Invalid API Key. Please check your API Key and try again.")
+#         return False
+
 def create_prompt(system_description, human_description, content):
     system_prompt = SystemMessagePromptTemplate.from_template(system_description)
     human_prompt = HumanMessagePromptTemplate.from_template(human_description)
@@ -24,32 +46,33 @@ def create_prompt(system_description, human_description, content):
     return chat_api.invoke(request).content
 
 if generate_pitch:
+    # if test_api_key(api_key):
     # Identify the pain point # Todo - add chat.stream to have a typewriter effect
     with st.spinner("Identifying the pain point..."):
         hardest_part = create_prompt(
-            system_description="You are an expert in identifying the hardest part of a job. Provide the hardest part of this job.",
+            system_description="You are an expert in identifying the hardest part of a job to be dobe. The user will give you a job-to-be-done and you need to identify the hardest part of the job-to-be-done. Provide the hardest part of this job.",
             human_description="The job-to-be-done is: {content}",
             content=job_description
         )
         st.markdown('**The hardest part of the job:**')
-        st.write(hardest_part)
+        st.write_stream(hardest_part)
 
     # Create the value proposition
     with st.spinner("Creating the value proposition..."):
         value_proposition = create_prompt(
-            system_description="Transform the hardest part of a job into a value proposition.",
+            system_description="You are an expert in transforming the hardest part of a job to be done into a value proposition. The user will give you the hardest part of a job-to-be-done and you need to transform the hardest part of the job-to-be-done into a value proposition.",
             human_description="The hardest part of the job-to-be-done is: {content}",
             content=hardest_part
         )
         st.markdown('**The value proposition:**')
-        st.write(value_proposition)
+        st.write_stream(value_proposition)
 
     # Generate the elevator pitch
     with st.spinner("Generating the elevator pitch..."):
         elevator_pitch = create_prompt(
-            system_description="Generate an elevator pitch from a value proposition.",
+            system_description="You are an expert in generating elevator pitches. The user will give you a value proposition and you need to generare an elevator pitch.",
             human_description="The value proposition is: {content}",
             content=value_proposition
         )
         st.markdown('**Here is the elevator pitch:**')
-        st.write(elevator_pitch)
+        st.write_stream(elevator_pitch)
